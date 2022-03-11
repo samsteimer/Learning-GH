d37876f2e12e5722b16825fcfb22a643bf69e19f
Sam Steimer <s_steimer@hotmail.com>
2022-03-11 13:44:50 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Sam Steimer on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
